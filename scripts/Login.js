f881f31ea1a9c4a1cc2d36f4b9c367de198250f3
Mahesh Suthar <147828642+MaheshSuthar01@users.noreply.github.com>
2024-10-11 17:57:19 +0530

    Add files via upload

---
+++ scripts/Login.js
@@ -0,0 +1,16 @@
+﻿let eye = document.getElementById("eye");
+let password = document.getElementById("password");
+
+eye.onclick = function () {
+    if (password.type == "password") {
+        password.type = "text";
+        this.classList.add("fa-eye");
+        this.classList.remove("fa-eye-slash");
+
+    } else {
+        password.type = "password";
+        this.classList.remove("fa-eye");
+        this.classList.add("fa-eye-slash");
+    }
+
+}
