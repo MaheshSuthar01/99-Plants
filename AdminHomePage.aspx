f7397cd78b482388d15eaa49ba591bde3a53c41c
Mahesh Suthar <147828642+MaheshSuthar01@users.noreply.github.com>
2024-10-11 18:01:02 +0530

    Add files via upload

---
+++ AdminHomePage.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminMasterPage.Master" AutoEventWireup="true" CodeBehind="AdminHomePage.aspx.cs" Inherits="PLANTS.AdminHomePage" EnableEventValidation="false"%>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <br />
+    <br />
+    <h1>Welcome Admin !...</h1>
+    
+</asp:Content>
---
+++ AdminMasterPage.Master.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PLANTS
+{
+    public partial class AdminMasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (Session["Username"] != null)
+            {
+                user.Text = Session["Username"].ToString();
+            }
+            else
+            {
+                Response.Redirect("~/Login_page/login.aspx");
+            }
+        }
+        //signout button
+        protected void btnAdmin_Click(object sender, EventArgs e)
+        {
+
+            Response.Redirect("~/Login_page/login.aspx");
+            Session["Username"] = null;
+        }
+        //manage button
+        protected void btnManage_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Manage/manage.aspx");
+        }
+    }
+}
